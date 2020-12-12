f103e45d58b19bf482b701b33b7aeb69b9bdbd1e
scrt squirrel_Stod_25 <chezling.stoddard@gmail.com>
2020-12-12 19:32:45 -0400

    Building table function
    Making the code to build out the table dymanic

--- designs.js
+++ designs.js
@@ -1,11 +1,13 @@
 // Select color input
 // Select size input
 let width, height;
-let color;
+let color = document.getElementById('colorpicker');
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
     // Your code goes here!
+
+    const gridhth = document.getElementById('inputHeight').value;
+    const gridhth = document.getElementById('inputWidth').value;
 
 }
