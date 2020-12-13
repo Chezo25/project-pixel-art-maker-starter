9096ac2b57c8b3dffd4da076256b20fd9b025b1e
scrt squirrel_Stod_25 <chezling.stoddard@gmail.com>
2020-12-13 12:57:46 -0400

    Table adjustments
    Fix dugs in the table code

--- designs.js
+++ designs.js
@@ -3,14 +3,31 @@
 let width, height, color;
 
 // When size is submitted by the user, call makeGrid()
-$('SizePicker').submit
+$('SizePicker').submit((evt) => {
+    evt.preventDefault();
+    height = $; { '#inputHeight'; }
+    val();
+    width = $; { '#inputWidth'; }
+    val();
+    makeGrid(width, height);
+    //console.log(`Height:${height}and width:${width}`);
+})
 
 
 
 
 
-function makeGrid() {
+function makeGrid(r, c) {
+    $('tr').remove();
+
     // Your code goes here!
+    let grid = document.getElementById('pixelCanvas');
 
+    for (let i = 1; i <= c; i++) {
+        $('#grid').append('<tr id=table' + i + '></tr>');
 
+        for (let x = 1; x <= r; x++) {
+            $('#table' + i).append('<td></td>');
+        }
+    }
 }
