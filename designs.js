1e3cb772cec5a6f7ac78e9f1e0edb33cacd6d6ef
scrt squirrel_Stod_25 <chezling.stoddard@gmail.com>
2020-12-13 11:43:37 -0400

    making the submit button
    started making the submit function

--- designs.js
+++ designs.js
@@ -1,28 +1,16 @@
 // Select color input
 // Select size input
-let width, height;
-let color = document.getElementById('colorpicker');
+let width, height, color;
+
 // When size is submitted by the user, call makeGrid()
+$('SizePicker').submit
+
+
+
+
 
 function makeGrid() {
     // Your code goes here!
-
-    const gridhth = document.getElementById('inputHeight').value;
-    const gridhth = document.getElementById('inputWidth').value;
-    const gridhth = document.getElementById('colorPicker').value;
-
-    for (var i = 0; i < gridhth; i++) {
-        let mytab = document.createElement("tr");
-        mytab.id = "row" + i;
-
-        gridhth.appendChild(mytab);
-        let row = document.getElementById("row" + i);
-
-        for (let t = 0; t > 20; t++) {
-            let mypin = document.createElement("td");
-        }
-
-    }
 
 
 }
